c64093d046ac040cb802cd8cfbd311c43b809eb0
Juste Raimbault <juste.raimbault@polytechnique.edu>
2019-03-13 11:25:14 +0100

    file_splitting_cuisine_for_mongo_import

--- library/data/osm/pbf2mongo.sh
+++ library/data/osm/pbf2mongo.sh
@@ -4,10 +4,15 @@ FILE=$1
 DB=$2
 
 # 1) osmosis : pbf -> filtered pbf
-osmosis --read-pbf $FILE --log-progress --tf accept-ways $TAG=* --used-node --write-pbf $FILE.tmp.pbf
+#osmosis --read-pbf $FILE --log-progress --tf accept-ways $TAG=* --used-node --write-pbf $FILE.tmp.pbf
+filterosm_pbf.sh $FILE
 
 # 2) pbf -> GeoJson
-ogr2ogr -f GeoJSON $FILE.tmp.json $FILE.tmp.pbf
+#ogr2ogr -f GeoJSON $FILE.tmp.json $FILE.tmp.pbf
+osmium $FILE.tmp.pbf -o $FILE.geojson
 
 # 3) mongoimport
-python loadMongo.py $FILE.tmp.json $DB
+# requires mongo running on localhost:27017
+python loadMongo.py $FILE.geojson $DB
+
+

4ef6bc7c4e7887bb35138773ae87b158e6e9a19b
Juste Raimbault <juste.raimbault@polytechnique.edu>
2020-08-27 18:37:13 +0100

    fix:compil

--- library/src/main/scala/org/openmole/spatialdata/utils/gis/PoligonizerUtils.scala
+++ library/src/main/scala/org/openmole/spatialdata/utils/gis/PoligonizerUtils.scala
@@ -9,49 +9,51 @@ import org.locationtech.jts.operation.polygonize.Polygonizer
 import org.locationtech.jts.precision.GeometryPrecisionReducer
 import org.openmole.spatialdata.utils
 
+import scala.collection.mutable.ArrayBuffer
 import scala.jdk.CollectionConverters._
 
 object PoligonizerUtils {
   private val fact = new GeometryFactory()
   var DEBUG = false
 
-  private def getLines(inputFeatures: Seq[Geometry]) = {
+  private def getLines(inputFeatures: Seq[Geometry]): Seq[Geometry] = {
     val linesList = new util.ArrayList[Geometry]()
     val lineFilter = new LinearComponentExtracter(linesList)
     for (feature <- inputFeatures) feature.apply(lineFilter)
-    linesList
+    linesList.asScala.toSeq
   }
 
-  private def extractPoint(lines: util.List[Geometry]) = {
-    lines.asScala.find(!_.isEmpty).map(g=>g.getFactory.createPoint(g.getCoordinate))
+  private def extractPoint(lines: Seq[Geometry]): Option[Point] = {
+    lines.find(!_.isEmpty).map(g=>g.getFactory.createPoint(g.getCoordinate))
   }
 
-  private def nodeLines(lines: util.List[Geometry]) = {
-    val linesGeom = fact.createMultiLineString(lines.toArray[LineString](new Array(lines.size())))
-    var unionInput = extractPoint(lines) match {
+  private def nodeLines(lines: Seq[Geometry]): Geometry = {
+    val linesarray = Array.tabulate(lines.length)(i => lines(i).asInstanceOf[LineString])
+    val linesGeom = fact.createMultiLineString(linesarray)
+    val unionInput = extractPoint(lines) match {
       case Some(p) => p
       case None => fact.createMultiLineString(null)
     }
     linesGeom.union(unionInput)
   }
 
-  private def addFeatures(p: Polygonizer, inputFeatures: Seq[Geometry], pm: PrecisionModel = new PrecisionModel(100)) = {
+  private def addFeatures(p: Polygonizer, inputFeatures: Seq[Geometry], pm: PrecisionModel = new PrecisionModel(100)): Unit = {
     //utils.log(s"${Calendar.getInstance.getTime} node lines")
     val reduced = inputFeatures.map(GeometryPrecisionReducer.reduce(_, pm))
     // extract linear components from input geometries
     val lines = getLines(reduced)
     // node all geometries together
-    val nodedLines = nodeLines(lines) match {
+    val nodedLines = nodeLines(lines.map(_.asInstanceOf[LineString])) match {
       case mls: MultiLineString =>
         val geoms = for (i <- 0 until mls.getNumGeometries) yield mls.getGeometryN(i)
-        nodeLines(geoms.asJava)
+        nodeLines(geoms)
       case ls: LineString => ls
     }
     //utils.log(s"${Calendar.getInstance.getTime} insert lines")
     p.add(nodedLines)
   }
 
-  def getPolygons(features: Seq[Geometry]) = {
+  def getPolygons(features: Seq[Geometry]): Seq[Geometry] = {
     val polygonizer = new Polygonizer
     addFeatures(polygonizer, features)
     if (DEBUG) println(s"${Calendar.getInstance.getTime} now with the real stuff")
@@ -60,7 +62,7 @@ object PoligonizerUtils {
     result
   }
 
-  def getPolygonIntersection(features: Seq[Geometry], mask: Geometry) = {
+  def getPolygonIntersection(features: Seq[Geometry], mask: Geometry): Seq[Polygon] = {
     val polygonizer = new Polygonizer
     addFeatures(polygonizer, features :+ mask)
     if (DEBUG) println(s"${Calendar.getInstance.getTime} now with the real stuff")
@@ -72,7 +74,7 @@ object PoligonizerUtils {
     result
   }
 
-  def getPolygonDifference(features: Seq[Geometry], mask: Geometry) = {
+  def getPolygonDifference(features: Seq[Geometry], mask: Geometry): Seq[Polygon] = {
     val polygonizer = new Polygonizer
     addFeatures(polygonizer, features :+ mask)
     if (DEBUG) println(s"${Calendar.getInstance.getTime} now with the real stuff")

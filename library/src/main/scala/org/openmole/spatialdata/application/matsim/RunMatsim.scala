e9ca7df79ace0efb0d1f1e6b69e821a5c81488d9
Juste Raimbault <juste.raimbault@polytechnique.edu>
2020-10-01 10:15:07 +0100

    .

--- library/src/main/scala/org/openmole/spatialdata/application/matsim/RunMatsim.scala
+++ library/src/main/scala/org/openmole/spatialdata/application/matsim/RunMatsim.scala
@@ -1,6 +1,7 @@
 package org.openmole.spatialdata.application.matsim
 
 import org.openmole.spatialdata.network.real.GISFileNetworkGenerator
+import org.openmole.spatialdata.utils.io.Gexf
 
 import scala.util.Random
 
@@ -10,4 +11,6 @@ object RunMatsim extends App {
   println("Data file: "+args(0))
   val nw = GISFileNetworkGenerator(args(0)).generateNetwork
   println(nw)
+  // export network to matsim format
+  Gexf.writeGexf(nw,"/data/outputs/network.gexf")
 }
